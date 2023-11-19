8bc8dea4f87729272ffcadac3ef4adb08eb52a2a
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-19 11:55:29 +0530

    added new done

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="p-5">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum facere
+      iste commodi reiciendis ipsa et molestias praesentium, esse, repudiandae
+      ab, excepturi possimus eaque aut magni est natus eligendi assumenda
+      ratione vero dolores perferendis nihil. Vero error accusamus deserunt
+      reiciendis qui molestias excepturi tenetur veritatis deleniti ducimus,
+      possimus ab beatae harum quidem quos magnam. Veritatis, debitis provident.
+    </div>
+  );
+}
